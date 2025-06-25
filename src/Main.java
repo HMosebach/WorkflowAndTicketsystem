52eb5ca6fc9e1a451ef69758bad0aa177ef3a938
HMosebach <omosebach@hotmail.de>
2025-06-25 17:05:56 +0200

    HD-3 Div 0

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,10 @@ public class Main {
         // Press Alt+Eingabe with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.println("Hello and welcome!");
-
+        int u =0;
         // Press Umschalt+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
-
+            //u=100/u;
             // Press Umschalt+F9 to start debugging your code. We have set one breakpoint
             // for you, but you can always add more by pressing Strg+F8.
             System.out.println("i = " + i);
