c9c4233059dde44da71225616f4c3c4667064d1f
HMosebach <omosebach@hotmail.de>
2025-06-25 17:54:09 +0200

    HD-5 Test Test Test

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,15 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Eingabe with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
+        // TEST
         System.out.println("Hello and welcome!");
         int u =0;
         // Press Umschalt+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
-            //u=100/u;
-            // Press Umschalt+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Strg+F8.
+
             System.out.println("i = " + i);
-            // hghghghghghg 55555555555555
-            /////// 000000007777000000000
-            //ugjgjgjgjgjgjgjgjgjgjg
+
         }
-        // khfk uggkugkugkufkjfkutgliilgk fdg fdg fdgfgdffdgdff 7777777
-        // Test+++++++2 000000111111111111111 7777777 77щшрлоло
-        // hgkjh lkjkhg jhv j,h v kjvh
-        // ghghgjgjgkhkjljljljl
-// hk gkjg kjg kjg kj gkj kj  hd2
+
     }
 }
